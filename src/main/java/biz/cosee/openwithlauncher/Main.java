6e47036da107fee0763a05cfdd809ea456b952af
Konstantin Diener <konstantin.diener@cosee.biz>
2018-05-06 12:53:10 +0200

    Implemented the sample's Main.main.

--- src/main/java/biz/cosee/openwithlauncher/Main.java
+++ src/main/java/biz/cosee/openwithlauncher/Main.java
@@ -1,8 +1,22 @@
 package biz.cosee.openwithlauncher;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
+    public static void main(String[] args) {
+        logger.info("Starting on OS '{}'.", System.getProperty("os.name"));
+
+        FileExtractor fileExtractor =
+                new FileExtractorFactory(System.getProperty("os.name")).createFileExtractor(args);
+
+        List<File> passedFiles = fileExtractor.listOfFilesPassedToTheApplication();
+        logger.info("Files to open: {}", passedFiles);
     }
 }
