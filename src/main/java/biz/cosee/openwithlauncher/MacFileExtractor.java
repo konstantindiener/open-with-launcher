66c71ad38992b497c427fad0e3273f0c47018e45
Konstantin Diener <konstantin.diener@cosee.biz>
2018-05-06 12:46:45 +0200

    Implemented MacFileExtractor. Added 'test' scope to all test dependencies. Added mockito framework.

--- src/main/java/biz/cosee/openwithlauncher/MacFileExtractor.java
+++ src/main/java/biz/cosee/openwithlauncher/MacFileExtractor.java
@@ -1,11 +1,40 @@
 package biz.cosee.openwithlauncher;
 
+import com.apple.eawt.AppEvent;
+import com.apple.eawt.Application;
+import com.apple.eawt.OpenFilesHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
+import java.util.Collections;
 import java.util.List;
 
-public class MacFileExtractor implements FileExtractor {
+public class MacFileExtractor implements FileExtractor, OpenFilesHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(MacFileExtractor.class);
+
+    private List<File> passedFiles = Collections.emptyList();
+
+    MacFileExtractor() {
+        logger.info("Registering MacFileExtractor as OpenFileHandler.");
+        Application.getApplication().setOpenFileHandler(this);
+    }
+
     @Override
     public List<File> listOfFilesPassedToTheApplication() {
-        return null;
+        return passedFiles;
+    }
+
+    @Override
+    public void openFiles(AppEvent.OpenFilesEvent openFilesEvent) {
+
+        passedFiles = openFilesEvent.getFiles();
+
+        logger.debug("Received an OpenFilesEvent ({}, {}, {}).",
+                openFilesEvent.getSearchTerm(),
+                openFilesEvent.getSource(),
+                passedFiles);
+
     }
 }
--- src/test/java/biz/cosee/openwithlauncher/MacFileExtractorTest.java
+++ src/test/java/biz/cosee/openwithlauncher/MacFileExtractorTest.java
@@ -1,10 +1,47 @@
 package biz.cosee.openwithlauncher;
 
+import com.apple.eawt.AppEvent;
 import org.junit.Test;
+
+import java.io.File;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class MacFileExtractorTest {
 
+    private static final File[] fileList = {new File("file1"), new File("file2")};
+
     @Test
-    public void listOfFilesPassedToTheApplication() {
+    public void listFilesAfterOSCallbackIsCalled() {
+
+        MacFileExtractor macFileExtractor = new MacFileExtractor();
+        macFileExtractor.openFiles(createMockedOpenFilesEvent());
+
+        List<File> passedFiles = macFileExtractor.listOfFilesPassedToTheApplication();
+        assertThat(passedFiles).contains(fileList);
+    }
+
+    private AppEvent.OpenFilesEvent createMockedOpenFilesEvent() {
+        AppEvent.OpenFilesEvent openFilesEvent = mock(AppEvent.OpenFilesEvent.class);
+
+        when(openFilesEvent.getSearchTerm())
+                .thenReturn("test");
+        when(openFilesEvent.getFiles())
+                .thenReturn(asList(fileList));
+        when(openFilesEvent.getSource())
+                .thenReturn("os");
+        return openFilesEvent;
+    }
+
+    @Test
+    public void listFilesWithoutOSCallback() {
+
+        List<File> passedFiles = new MacFileExtractor().listOfFilesPassedToTheApplication();
+
+        assertThat(passedFiles).isEmpty();
     }
 }
