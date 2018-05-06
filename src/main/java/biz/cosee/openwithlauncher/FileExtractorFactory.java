92e9bf92d06b85b8cb127a7ae247e8d6105fbe22
Konstantin Diener <konstantin.diener@cosee.biz>
2018-05-06 12:19:53 +0200

    Added logging to the FileExtractorFactory.

--- src/main/java/biz/cosee/openwithlauncher/FileExtractorFactory.java
+++ src/main/java/biz/cosee/openwithlauncher/FileExtractorFactory.java
@@ -1,6 +1,11 @@
 package biz.cosee.openwithlauncher;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 class FileExtractorFactory {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileExtractorFactory.class);
 
     private final String operatingSystemName;
 
@@ -11,9 +16,11 @@ class FileExtractorFactory {
     FileExtractor createFileExtractor(String[] commandLineArguments) {
 
         if (operatingSystemName.toLowerCase().contains("mac os")) {
+            logger.info("Creating MacFileExtractor");
             return new MacFileExtractor();
         }
 
+        logger.info("Creating WindowsFileExtractor");
         return new WindowsFileExtractor(commandLineArguments);
     }
 }
